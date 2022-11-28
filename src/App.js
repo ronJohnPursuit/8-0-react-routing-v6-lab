bcb2a5540c3e117a418d539d11a527b6c72954b1
ronJohnPursuit <ronjohnson@pursuit.org>
2022-11-28 09:39:02 -0500

    pass

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 /*
   Components
@@ -25,11 +26,26 @@ function App() {
 
   return (
     <div className="wrapper">
-      <Nav />
-      <Home employees={employees} owners={owners} pets={pets} />
-      <StaffList employees={employees} />
-      <PetsList pets={pets} />
-      <Footer />
+      <Router>
+        <Nav />
+        <Routes>
+          <Route
+            path="/"
+            element={<Home employees={employees} owners={owners} pets={pets} />}
+          />
+          <Route path="/staff" element={<StaffList employees={employees} />} />
+          <Route path="/pets" element={<PetsList pets={pets} kind={"all"} />} />
+          <Route
+            path="/pets/cats"
+            element={<PetsList pets={pets} kind={"cats"} />}
+          />
+          <Route
+            path="/pets/dogs"
+            element={<PetsList pets={pets} kind={"dogs"} />}
+          />
+        </Routes>
+        <Footer />
+      </Router>
     </div>
   );
 }
--- src/components/pets/PetsList.js
+++ src/components/pets/PetsList.js
@@ -1,8 +1,12 @@
 import PetsListNav from "./PetsListNav";
 import Pet from "./Pet";
 import "./PetsList.css";
+import { Link, Params, useRoutes, useNavigate } from "react-router-dom";
+import { useEffect } from "react";
 
-export const PetsList = ({ pets }) => {
+export const PetsList = ({ pets, kind }) => {
+  // const { id } = useParams();
+
   const [cats, dogs] = pets.reduce(
     (acc, pet) => {
       const position = pet.kind === "Cat" ? 0 : 1;
@@ -12,19 +16,25 @@ export const PetsList = ({ pets }) => {
     [[], []]
   );
 
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (kind === "all") {
+      navigate("/pets/cats");
+    }
+  }, [pets]);
+
   return (
     <section className="pets-wrapper">
       <PetsListNav cats={cats} dogs={dogs} />
       <section className="pets-list">
         {/* All cats section */}
-        {cats.map((cat) => (
-          <Pet key={cat.id} kind="cat" pet={cat} />
-        ))}
+        {kind !== "dogs" &&
+          cats.map((cat) => <Pet key={cat.id} kind="cat" pet={cat} />)}
 
         {/* All dogs section */}
-        {dogs.map((dog) => (
-          <Pet key={dog.id} kind="dog" pet={dog} />
-        ))}
+        {kind !== "cats" &&
+          dogs.map((dog) => <Pet key={dog.id} kind="dog" pet={dog} />)}
       </section>
     </section>
   );
